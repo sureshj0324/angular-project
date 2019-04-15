3e1cba6cb06b4f0348ab733e1c65bbbb62e717cd
sureshj0324 <sureshjrd21@gmail.com>
2019-04-15 11:28:20 +0530

    Add css to display the login ui to customers

--- src/app/login-routing.module.ts
+++ src/app/login-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PostsComponent } from './posts/posts.component';
+
+const routes: Routes = [
+  { path: '', redirectTo : '/login', pathMatch:'full' },
+  { path: 'login', component:LoginComponent },
+  { path: 'posts', component:PostsComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class LoginRoutingModule { }
